d890d2bab1b44fe1db13a33d99d6ea0f7afd5f12
Emmanuel Simpson <57595259+emmanuel260897@users.noreply.github.com>
2019-11-27 22:19:05 +0800

    Added Email in SQL File
    At line 36, i added an attribute for email.

--- dominos.sql
+++ dominos.sql
@@ -33,6 +33,7 @@ CREATE TABLE `customer` (
   `CName` varchar(50) NOT NULL,
   `TPNo` int(10) NOT NULL,
   `Address` varchar(100) NOT NULL
+  `Email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
